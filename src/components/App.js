4a1d67b00931e6ff098b0424a0657937d3d890cd
jpra2021 <jthvmcm2021@cuk.edu>
2022-09-26 03:19:52 +0900

    w2-step1-setting

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from "logo.svg";
+import "App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Web Portfolio</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
